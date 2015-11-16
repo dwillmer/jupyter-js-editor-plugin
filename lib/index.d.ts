19219273c4a82de21cc4ad680a8acc2d2497149b
Dave Willmer <dave.willmer@gmail.com>
2015-11-16 16:40:56 +0000

    Cleanup

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Plugin loader function for the menu.
+ */
+export declare function menuLoader(extension: any): Promise<any>;
+/**
+ * Plugin loader function for the UI items.
+ */
+export declare function uiLoader(extension: any): Promise<any>;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 'use strict';
+
+import {
+  EditorModel, EditorWidget
+} from 'jupyter-js-editor';
+
+import {
+  Tab
+} from 'phosphor-tabs';
+
+
+var MENU = {
+  items: [
+    {
+      location: ["New", "Editor"],
+      command: "jupyter.new.editor"
+    }
+  ]
+};
+
+/**
+ * Plugin loader function for the menu.
+ */
+export
+function menuLoader(extension:any): Promise<any> {
+  return Promise.resolve(MENU);
+}
+
+/**
+ * Plugin loader function for the UI items.
+ */
+export
+function uiLoader(extension: any): Promise<any> {
+  var model = new EditorModel();
+  var widget = new EditorWidget(model);
+  var ui = {
+    items: [widget],
+    tabs: [new Tab('Editor')]
+  };
+  return Promise.resolve(ui);
+}
